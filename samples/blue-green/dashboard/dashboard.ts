76d1e90a1b4341ef25a5c572366c688a2a09cc5d
Dan Wahlin <danwahlin@iMac-Office.local>
2020-02-05 00:46:38 -0700

    Added prototype blue-green console dashboard

---
+++ samples/blue-green/dashboard/dashboard.ts
@@ -0,0 +1,71 @@
+import * as k8s from '@kubernetes/client-node';
+import * as Table from 'easy-table';
+
+const kc = new k8s.KubeConfig();
+kc.loadFromDefault();
+
+const appsV1Api = kc.makeApiClient(k8s.AppsV1Api);
+const coreV1Api = kc.makeApiClient(k8s.CoreV1Api);
+const roles = ['blue', 'green'];
+
+appsV1Api.listNamespacedDeployment('default').then(res => {
+    let deployments = [];
+    for (const deployment of res.body.items) {
+        let role = deployment.spec.template.metadata.labels.role;
+        if (role && roles.includes(role)) {
+            deployments.push({ 
+                name: deployment.metadata.name, 
+                role,
+                status: deployment.status.conditions[0].status,
+                image: deployment.spec.template.spec.containers[0].image,
+                ports: [],
+                services: []
+            });
+        }
+    }
+    return deployments;
+})
+.then(deployments => {
+    coreV1Api.listNamespacedService('default').then(res => {
+        for (const service of res.body.items) {
+            if (service.spec.selector && service.spec.selector.role && roles.includes(service.spec.selector.role)) {
+                let filteredDeployments = deployments.filter(d => {
+                    return d.role === service.spec.selector.role;
+                });
+                if (filteredDeployments) {
+                    for (const d of filteredDeployments) {
+                        d.ports.push(service.spec.ports[0].port);
+                        d.services.push(service.metadata.name);
+                    }
+                }
+            }
+        }
+
+        renderTable(deployments);
+    });
+});
+
+function renderTable(data, showHeader = true) {
+    const table = new Table();
+    for (const d of data) {
+        d.services.sort();
+        d.ports.sort();
+        table.cell('Deployment', d.name);
+        table.cell('Role', d.role);
+        table.cell('Status', d.status ? 'running' : 'stopped');
+        table.cell('Image', d.image);
+        table.cell('Ports', d.ports.join(', '));
+        table.cell('Services', d.services.join(', '))
+        table.newRow();
+    };
+    table.sort(['Role|asc']);
+    if (showHeader) {
+        console.log(table.toString());
+    } else {
+        console.log(table.print());
+    }
+}
+
+function filterByRole(deployments, role) {
+    return deployments.filter(d => d.role === role);
+}
---
+++ samples/blue-green/dashboard/readme.md
@@ -0,0 +1,17 @@
+## Instructions
+
+This project can be used to display a simple console-based dashboard for Blue-Green Deployments and their associated services.
+
+```
+Deployment              Role   Status   Image        Ports     Services                      
+----------------------  -----  -------  -----------  --------  ----------------
+nginx-deployment-blue   blue   running  nginx-blue   80, 9000  nginx-blue-test, nginx-service
+nginx-deployment-green  green  running  nginx-green  9001      nginx-green-test
+```
+
+1. Run `npm install`
+1. Run `npm run build`
+1. Run `npm start`
+
+Based on the https://github.com/kubernetes-client/javascript api.
+
